2f4bfbb3ff56ae1b6ad51807bbe1c2122017b735
Bram <Bram.mathijssen@gmail.com>
2022-11-22 21:29:44 +0100

    calling entranceFee function through nextjs with useWeb3Contract hook

--- frontend-lottery/components/Header.jsx
+++ frontend-lottery/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ConnectButton } from "@web3uikit/web3";
 
 const Header = () => {
-  return <ConnectButton moralisAuth={false} />;
+  return <ConnectButton moralisAuth={false} />; 
 };
 
 export default Header;
--- frontend-lottery/components/LotteryEntrance.jsx
+++ frontend-lottery/components/LotteryEntrance.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import { useWeb3Contract } from 'react-moralis'
+import { useEffect, useState } from "react";
+import { useWeb3Contract } from "react-moralis";
+import { contractAddresses, abi } from "../constants";
+import { useMoralis } from "react-moralis";
 
 const LotteryEntrance = () => {
-    const {runContractFunction: enterRaffle} = useWeb3Contract({
-        
-    })
-  return (
-    <div>LotteryEntrance</div>
-  )
-}
+  const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis();
+  // These get re-rendered every time due to our connect button!
+  const chainId = parseInt(chainIdHex);
+  // console.log(`ChainId is ${chainId}`)
+  const raffleAddress =
+    chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+  const [entranceFee, setEntranceFee] = useState("0");
 
-export default LotteryEntrance
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  // reads the raffle entrance fee if web3 is enabled
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      // pattern om async function in een useEffect te gebruiken
+      async function updateUI() {
+        const entranceFeeFromContract = (await getEntranceFee()).toString();
+        setEntranceFee(entranceFeeFromContract);
+        console.log(`entrance fee from contract is ${entranceFeeFromContract}`);
+      }
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+
+  return (
+    <div>
+      <h1>LotteryEntrance</h1>
+      <p>The entrance fee is: {entranceFee}</p>
+    </div>
+  );
+};
+
+export default LotteryEntrance;
